b45b963e51bfab55c31f99d491f7fdf777ef8202
TNZ <92765882+tehau26nz@users.noreply.github.com>
2023-05-04 15:00:39 +1200

    Changed title

--- index.js
+++ index.js
@@ -3,8 +3,11 @@ var numberOfDrumButtons = document.querySelectorAll(".drum").length;
 for (var i = 0; i<numberOfDrumButtons; i++){
 
     document.querySelectorAll(".drum")[i].addEventListener("click", function (){
-        var audio = new Audio("sounds/tom-1.mp3");
-        audio.play();
+        console.log(this);
     });
 
 }
+
+
+// var audio = new Audio("sounds/tom-1.mp3");
+//         audio.play();
