39306f3768cfc84ad6f0d252670de9ce63306061
TNZ <92765882+tehau26nz@users.noreply.github.com>
2023-05-04 14:04:18 +1200

    Improved sound to clicks code

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var numberOfDrumButtons = document.querySelectorAll(".drum").length;
 for (var i = 0; i<numberOfDrumButtons; i++){
 
     document.querySelectorAll(".drum")[i].addEventListener("click", function (){
-        var audio = new Audio('/sounds/tom-1.mp3');
+        var audio = new Audio("sounds/tom-1.mp3");
         audio.play();
     });
 
